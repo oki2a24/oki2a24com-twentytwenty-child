fd25410a3062df59f5e0d08f3ec736d1aa6d914a
oki2a24 <oki2a24@gmail.com>
2019-11-27 08:44:09 +0900

    fix: WordPressコーディング規約チェックツールに従って修正する

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+/**
+ * Oki2a24.com, Twenty Twenty Child の関数と定義
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package oki2a24.com,_Twenty_Twenty_Child
+ * @since 1.0.0
+ */
+
+/**
+ * スタイルを登録し、エンキューします。
+ */
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $theme_version );
 }
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
